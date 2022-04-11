827954680275b28e5384e17f4f2f2a92b16bb76c
Morné Louw <louw.morne@gmail.com>
2022-04-11 16:02:48 +0200

    useEffect - Adding a Cleanup function

--- src/components/UseEffectComponent.tsx
+++ src/components/UseEffectComponent.tsx
@@ -6,12 +6,16 @@ const UseEffectComponent = () => {
     useEffect(() => {
         console.log("[UseEffectComponent]: useEffect is executed");
 
-        setInterval(() => {
+        const timer = setInterval(() => {
             console.log(
                 "[UseEffectComponent]: useEffect > setInterval is executed"
             );
             setVal((currentVal) => currentVal + 1);
         }, 1000);
+
+        // Here we return a clean-up function. This runs when this components dismounts.
+        // In this case we tell it to delete the timer so it does not hang around in memory after the are done with thos component.
+        return () => window.clearInterval(timer);
     }, []);
 
     return <div>{val}</div>;
