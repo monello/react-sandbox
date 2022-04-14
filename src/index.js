e039eccfa908320ec319b99f1ccfa2145033bce8
Morné Louw <louw.morne@gmail.com>
2022-04-14 12:51:36 +0200

    Very basic TS script

---
+++ src/index.js
@@ -0,0 +1,3 @@
+var multiply = function (a, b) { return a * b; };
+var result = multiply(2, 2);
+console.log("Result: ".concat(result));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const multiply = (a: number, b: number): number => a * b;
+
+const result = multiply(2, 2);
+console.log(`Result: ${result}`);
