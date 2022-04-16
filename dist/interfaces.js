7330288c075d13b9e15739f90d4133be3f8adb2d
Morné Louw <louw.morne@gmail.com>
2022-04-16 10:03:39 +0200

    Interface Basics - props: required, optional and readonly

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,6 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const interfaceBasics = () => {
+    // use the Profile interface to create a new profile object
+    let profileA = {
+        // Add all the required properties, this example shows that 'age' can be skipper
+        id: 'p001',
+        name: 'John'
+    };
+    let profileB = {
+        id: 'p002',
+        name: 'Cindy',
+        age: 24 // here an oprional age is added
+    };
+    // You can change non-readonly properties on-the-fly
+    profileA.name = 'Peter';
+    profileA.age = 40;
+    // TS sill stop you from trying to update the id prop due to readonly flag
+    // profileA.id = 'p003'
+    console.log('profileA:', profileA);
+    console.log('profileB:', profileB);
+};
 const go = () => {
-    console.log('Hello from go()');
+    interfaceBasics();
 };
 exports.default = go;
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,37 @@
-const go = () => {
-    console.log('Hello from go()');
+// Interfaces
+interface Profile {
+    name: string;        // Required property
+    age?: number;        // Oprional property
+    readonly id: string; // Readonly and required property
+}
 
+const interfaceBasics = () => {
+    // use the Profile interface to create a new profile object
+    let profileA: Profile = {
+        // Add all the required properties, this example shows that 'age' can be skipper
+        id: 'p001',
+        name: 'John'
+    };
+
+    let profileB: Profile = {
+        id: 'p002',
+        name: 'Cindy',
+        age: 24             // here an oprional age is added
+    };
+
+    // You can change non-readonly properties on-the-fly
+    profileA.name = 'Peter';
+    profileA.age = 40;
+
+    // TS sill stop you from trying to update the id prop due to readonly flag
+    // profileA.id = 'p003'
+
+    console.log('profileA:', profileA);
+    console.log('profileB:', profileB);
+};
+
+const go = () => {
+    interfaceBasics();
 };
 
 export default go;
