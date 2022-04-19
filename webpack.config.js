498e2655dc2abbd41e08d1ee520f894bb8ffe4f6
Morné Louw <louw.morne@gmail.com>
2022-04-19 15:58:11 +0200

    Inline comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html'
+            template: './public/index.html',    // The path to the HTML file that Webpack must use a s a template to generate the final entry HTML file
+            filename: 'index.html'              // Here we specify what we want Webpack to call the final HTML file
         })
     ]
 };
