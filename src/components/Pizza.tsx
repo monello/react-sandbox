3e5d6e9cb10c0c240c5f5c0f78a6ef830f070260
Morné Louw <louw.morne@gmail.com>
2022-04-21 08:40:04 +0200

    35. Functional Components - Create a Pizza component to render individual pizzas

---
+++ src/components/Pizza.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// 1. Describe a Pizza object
+interface Pizza {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+}
+
+// 2. Describe the props that this component will expect
+interface Props {
+    pizza: Pizza;
+}
+
+// 3. Use the Props interface to tell TS what the structure of the props should be by passing the interface to the React.FC Generic
+// 4 . Destructure the props-object as "pizza"
+const Pizza: React.FC<Props> = ({ pizza }) => {
+    return (
+        <li>
+            <h2>{pizza.name}</h2>
+            <p>{pizza.description}</p>
+            <p>{pizza.price}</p>
+        </li>
+    );
+};
+
+export default Pizza;
