dd47783936a6c6251ca960ef3edc7ece17d61b3d
Morné Louw <louw.morne@gmail.com>
2022-04-21 08:28:47 +0200

    35. Functional Components - Enable JSON file support

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+// with the help of "resolveJsonModule" and "moduleResolution" settings in the .tsconfig.json TS can not read and understand the JSON file and also
+//  build an object of it with the data in it
+import pizzas from "../data/pizzas.json";
 
 const App = () => {
     return <div>Hello World (App component)</div>;
