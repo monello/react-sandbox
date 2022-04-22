be271f26cb81319b2d64ab34209f069afe035253
Morné Louw <louw.morne@gmail.com>
2022-04-22 07:00:51 +0200

    36. Setting up CSS Modules - install and configure mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.tsx',  // Tell webpack where to fine the entry file (The file is starts building it's import-tree from), by default is looks in the 'src' dir for an index.js file, but we are now switching to TS
@@ -14,7 +15,10 @@ module.exports = {
             },
             {
                 test: /.css$/,
-                loader: 'css-loader'
+                use: [  // the "loader" property is used to use a single loader, here we want to use multiple loader so we change it to a "use" array.
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader' }
+                ]
             }
         ],
     },
@@ -23,6 +27,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',    // The path to the HTML file that Webpack must use a s a template to generate the final entry HTML file
             filename: 'index.html'              // Here we specify what we want Webpack to call the final HTML file
-        })
+        }),
+        new MiniCssExtractPlugin()              // Initilizing this plugin is straight-forward
     ]
 };
